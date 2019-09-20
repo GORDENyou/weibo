afa1642882a403bba0bc61ef97746e5d7a26f16a
Gordenyou <1193688859@qq.com>
2019-09-20 09:11:02 +0000

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,15 @@
 @extends('layouts.default')
 @section('content')
-  <h1>主页</h1>
+  <div class="jumbotron">
+    <h1>Hello Laravel</h1>
+    <p class="lead">
+      当前是 <a href="https://learnku.com/coutses/laravel-essential-training">Laravel 入门？教程</a> 主页。
+    </p>
+    <p>
+      一切的一切，将从这里开始。
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+    </p>
+  </div>
 @stop
